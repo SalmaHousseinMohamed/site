0b43727e1e2192c116af2c955f9833bfa3708a4d
unknown <salmamacash@gmail.com>
2024-01-14 14:04:21 +0800

    hosting on vercel.com

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -132,20 +132,21 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
+# Specify the directories where Django will look for static files
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    os.path.join(BASE_DIR, 'static')
 ]
 
-MEDIA_ROOT = BASE_DIR / 'static/images'
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+# Specify the directory where 'collectstatic' will gather static files for deployment
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
+# Specify the directory where media files will be stored
+MEDIA_ROOT = BASE_DIR / 'static/images'
+
+# Default primary key field type
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
