336cef790a29f0336cc429af5a5f1b2dbfa420d6
unknown <salmamacash@gmail.com>
2024-01-15 18:47:56 +0800

    hosting vercel.com

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -149,5 +149,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,  'static'),
 ]
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
